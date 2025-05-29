99f49c524c1debd0a915f7a924b2813250f47036
DESKTOP-D1BUJAF\a <wlsgksvheh1@naver.com>
2025-05-29 09:47:26 +0900

    강의 메모 추가 및 자바 쇼핑몰 수업 시작

---
+++ back_workspace/java_workspace/TableProject/src/shop/ShopMain.java
@@ -0,0 +1,58 @@
+package shop;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ShopMain extends JFrame{
+	
+	JPanel p_north ; //북쪽 패널
+	JButton bt_home;
+	JButton bt_product;
+	JButton bt_mypage;
+	JButton bt_cs;
+	
+	JPanel p_center;
+	
+	public ShopMain() {
+		//생성
+		p_north = new JPanel();
+		
+		bt_home = new JButton();
+		bt_product = new JButton();
+		bt_mypage = new JButton();
+		bt_cs = new JButton();
+		
+		p_center = new JPanel();
+			
+		//스타일
+		p_north.setPreferredSize(new Dimension(800,50));
+		p_north.setBackground(Color.YELLOW);
+		
+		
+		//조립
+		p_north.add(bt_home);
+		p_north.add(bt_product);
+		p_north.add(bt_mypage);
+		p_north.add(bt_cs);
+		add(p_north, BorderLayout.NORTH);
+		add(p_center);
+		
+		
+		
+		setSize(800,650);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);;
+	}
+
+	public static void main(String[] args) {
+		new ShopMain();
+
+	}
+
+}
