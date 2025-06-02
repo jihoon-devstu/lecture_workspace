48c30245f0098a134228a10acfcd092724a24741
DESKTOP-D1BUJAF\a <wlsgksvheh1@naver.com>
2025-06-02 09:51:30 +0900

    1교시 수업

---
+++ back_workspace/java_workspace/dbproject/src/main/java/com/sinse/dbproject/EmpLoad.java
@@ -0,0 +1,54 @@
+package com.sinse.dbproject;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+
+public class EmpLoad extends JFrame{
+	
+	JPanel p_north;
+	JButton bt_emp;
+	JButton bt_dept;
+	
+	JTable table;
+	JScrollPane scroll;
+	MyActionListener myActionListener;
+	
+	public EmpLoad() {
+		
+		p_north = new JPanel();
+		bt_emp = new JButton("사원 테이블 로드");
+		bt_dept = new JButton("부서 테이블 로드");
+		
+		table = new JTable();
+		scroll = new JScrollPane(table);
+		
+		myActionListener = new MyActionListener();
+		
+		//style	
+		p_north.setPreferredSize(new Dimension(800,30));
+		
+		//assemble
+		p_north.add(bt_emp);
+		p_north.add(bt_dept);
+		add(p_north, BorderLayout.NORTH);
+		add(scroll);
+		
+		setSize(800,630);
+		setVisible(true);
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		new EmpLoad();
+	}
+	
+}
