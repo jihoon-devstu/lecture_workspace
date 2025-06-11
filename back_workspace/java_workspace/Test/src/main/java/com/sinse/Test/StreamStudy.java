b2a91de754430fadcbf2c1ca35e34f003266b077
DESKTOP-D1BUJAF\a <wlsgksvheh1@naver.com>
2025-06-11 15:08:58 +0900

    2교시 커밋

--- back_workspace/java_workspace/Test/src/main/java/com/sinse/Test/StreamStudy.java
+++ back_workspace/java_workspace/Test/src/main/java/com/sinse/Test/StreamStudy.java
@@ -1,5 +1,6 @@
 package com.sinse.Test;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -44,7 +45,7 @@ public class StreamStudy {
 	// 문자 기반의 스트림으로 읽기
 	public void readReader() {
 		FileInputStream fis = null; // 바이트 기반
-		InputStreamReader reader;
+		InputStreamReader reader = null;
 
 		try {
 			fis = new FileInputStream(path);
@@ -78,11 +79,44 @@ public class StreamStudy {
 
 	// 버퍼 기반의 스트림으로 읽기
 	public void readBuffer() {
+		FileInputStream fis = null;
+		InputStreamReader reader = null;
+		BufferedReader buffr = null;
+		
+		String data = null;
+		
+		try {
+			fis = new FileInputStream(path);
+			reader = new InputStreamReader(fis);
+			buffr = new BufferedReader(reader);
+			
+			while(true) {
+				data = buffr.readLine();
+				
+				if(data == null)break;
+				System.out.println(data);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			if(fis !=null) {
+				try {
+					fis.close();
+				}catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 
 	}
 
 	public static void main(String[] args) {
 		StreamStudy s = new StreamStudy();
-		s.readReader();
+		s.readBuffer();
 	}
 }
