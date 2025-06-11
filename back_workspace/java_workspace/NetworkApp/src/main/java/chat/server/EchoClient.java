0dfda64e26267ac54d8d0fdb75d9ad35230bfe55
DESKTOP-D1BUJAF\a <wlsgksvheh1@naver.com>
2025-06-11 16:42:19 +0900

    오후 수업 종료 , 이후는 프로젝트

--- back_workspace/java_workspace/NetworkApp/src/main/java/chat/server/EchoClient.java
+++ back_workspace/java_workspace/NetworkApp/src/main/java/chat/server/EchoClient.java
@@ -23,102 +23,99 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class EchoClient extends JFrame implements Runnable{
+public class EchoClient extends JFrame implements Runnable {
 
 	JPanel p_north;
-	
+
 	JComboBox box_ip;
 	JTextField t_port;
 	JButton bt;
-	
+
 	JTextArea area;
 	JScrollPane scroll;
 	JTextField t_input;
-	
+
 	BufferedWriter buffw;
-	
+
 	BufferedReader buffr;
-	
+
 	Thread thread;
-	
-	/*대화용 소켓.. 이 객체를 메모리에 올릴 때 접속이 발생함
-	  또한 접속이 성공되면 , 그 시점부터 연결이 이루어진 것이므로 스트림을 통해 
-	  데이터를 주고받을 수 있음.*/
-	Socket socket; 
-	
+
+	/*
+	 * 대화용 소켓.. 이 객체를 메모리에 올릴 때 접속이 발생함 또한 접속이 성공되면 , 그 시점부터 연결이 이루어진 것이므로 스트림을 통해
+	 * 데이터를 주고받을 수 있음.
+	 */
+	Socket socket;
+
 	public EchoClient() {
-		//생성
+		// 생성
 		p_north = new JPanel();
-		
+
 		box_ip = new JComboBox();
 		t_port = new JTextField("9999", 8);
 		bt = new JButton("접속");
 		area = new JTextArea();
 		scroll = new JScrollPane(area);
 		t_input = new JTextField();
-		
+
 		createIp();
-		//스타일
+		// 스타일
 		area.setBackground(Color.yellow);
-		p_north.setPreferredSize(new Dimension(300,40));
-		box_ip.setPreferredSize(new Dimension(200,30));
-		t_port.setPreferredSize(new Dimension(60,30));
-		area.setPreferredSize(new Dimension(300,220));
-		t_input.setPreferredSize(new Dimension(300,40));
-		
-		
-		//조립
-		
+		p_north.setPreferredSize(new Dimension(300, 40));
+		box_ip.setPreferredSize(new Dimension(200, 30));
+		t_port.setPreferredSize(new Dimension(60, 30));
+		area.setPreferredSize(new Dimension(300, 220));
+		t_input.setPreferredSize(new Dimension(300, 40));
+
+		// 조립
+
 		p_north.add(box_ip);
 		p_north.add(t_port);
 		p_north.add(bt);
-		
+
 		add(p_north, BorderLayout.NORTH);
 		add(scroll);
 		add(t_input, BorderLayout.SOUTH);
-		
-		//접속 버튼과 리스너 연결
-		bt.addActionListener(e->{
+
+		// 접속 버튼과 리스너 연결
+		bt.addActionListener(e -> {
 			thread = new Thread(EchoClient.this);
 			thread.start();
 		});
-		
+
 		t_input.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyReleased(KeyEvent e) {
-				if(e.getKeyCode()==KeyEvent.VK_ENTER) {
-					//서버로 내보내기 !!(출력)
-					String msg = t_input.getText();
-					send(msg);
-					t_input.setText("");
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					// 서버로 내보내기 !!(출력)
+					send();
+					listen();
 				}
 			}
 		});
-		
-		
-		setSize(450,500);
+
+		setSize(450, 500);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	
+
 	public void connect() {
-	//소켓 서버 접속해보기
-		String ip = (String)box_ip.getSelectedItem();
+		// 소켓 서버 접속해보기
+		String ip = (String) box_ip.getSelectedItem();
 		int port = Integer.parseInt(t_port.getText());
-		
+
 		try {
-			socket = new Socket(ip,port);
-			
-			//소켓으로부터 스트림을 얻어오자
-			
-			//바이트 기반 , 출력 스트림(말하기)...
+			socket = new Socket(ip, port);
+			area.append("접속 성공\n");
+			// 소켓으로부터 스트림을 얻어오자
+
+			// 바이트 기반 , 출력 스트림(말하기)...
 			buffw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-					
+
 			// 바이트 기반 , 입력 스트림(듣기)
 			buffr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			
-			
-		}catch(NumberFormatException e) {
+
+		} catch (NumberFormatException e) {
 			e.printStackTrace();
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
@@ -127,31 +124,49 @@ public class EchoClient extends JFrame implements Runnable{
 		}
 	}
 	
-	//실행중인 프로그램에서 , 데이터를 내보내야 하므로 , 필요한 스트림은 바로 출력 스트림 !! 	
-	public void send(String msg) {
-		//서버로 한줄 보내기..
+	public void listen() {
+		String msg = null;
+		
 		try {
-			buffw.write(msg+"\n"); //보내기....
+			msg = buffr.readLine();
+			area.append(msg + "\n"); //로그 남기기
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
+		}
+		
 	}
-	
+
+	// 실행중인 프로그램에서 , 데이터를 내보내야 하므로 , 필요한 스트림은 바로 출력 스트림 !!
+	public void send() {
+		// 서버로 한줄 보내기..
+		String msg = t_input.getText();
+		try {
+			buffw.write(msg + "\n"); // 보내기....
+			//area.append(msg + "\n");
+			//버퍼 처리 된 출력스트림 계열을 데이터 전송 시 버퍼를 비워야 함.
+			buffw.flush();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		t_input.setText(""); //기존 입력내용 비우기
+	}
+
 	@Override
 	public void run() {
-		
+
 		connect();
 	}
-	
+
 	public void createIp() {
-		for(int i=15;i<=47;i++) {
-			box_ip.addItem("192.168.60."+i);
+		for (int i = 15; i <= 60; i++) {
+			box_ip.addItem("192.168.60." + i);
 		}
 	}
-	
 
-	
 	public static void main(String[] args) {
 		new EchoClient();
 	}
--- back_workspace/java_workspace/NetworkApp/src/main/java/chat/server/EchoGUIServer.java
+++ back_workspace/java_workspace/NetworkApp/src/main/java/chat/server/EchoGUIServer.java
@@ -2,7 +2,11 @@ package chat.server;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,56 +17,73 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class EchoGUIServer extends JFrame implements Runnable{
+public class EchoGUIServer extends JFrame implements Runnable {
 	JPanel p_north;
 	JTextField t_port;
 	JButton bt;
 	JTextArea area;
 	JScrollPane scroll;
-	
+
 	ServerSocket server;
-	
-	//Runnable은 쓰레드가 아니므로 , Runnable을 구현한다고 하여 , 이 객체가 쓰레드형이라고 오해하면 안됨.
-	//따라서 별도의 Thread 객체를 사용해야함... 단지 해당 Thread의 run메서드를 내가 가진 것 뿐임.
-	Thread thread; 
-	
+
+	// Runnable은 쓰레드가 아니므로 , Runnable을 구현한다고 하여 , 이 객체가 쓰레드형이라고 오해하면 안됨.
+	// 따라서 별도의 Thread 객체를 사용해야함... 단지 해당 Thread의 run메서드를 내가 가진 것 뿐임.
+	Thread thread;
+
+	// 클라이언트와의 대화를 위한 스트림 준비
+
+	BufferedReader buffr; // 듣기용
+	BufferedWriter buffw; // 말하기용
+
 	public EchoGUIServer() {
 		p_north = new JPanel();
 		t_port = new JTextField(8);
 		bt = new JButton("서버 가동");
 		area = new JTextArea();
 		scroll = new JScrollPane(area);
-		
+
 		area.setBackground(Color.yellow);
-		
+
 		p_north.add(t_port);
 		p_north.add(bt);
-	
-		
+
 		add(p_north, BorderLayout.NORTH);
 		add(scroll);
-		
-		bt.addActionListener(e->{
+
+		bt.addActionListener(e -> {
 			thread = new Thread(EchoGUIServer.this);
 			thread.start();
 		});
-		
-		
-	
-		setBounds(400, 100, 300,400);
+
+		setBounds(400, 100, 300, 400);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	
+
 	public void runServer() {
-		
+
 		try {
 			server = new ServerSocket(Integer.parseInt(t_port.getText()));
 			area.append("서버 생성 및 접속자 청취중..\n");
-			
+
 			Socket socket = server.accept();
 			String ip = socket.getInetAddress().getHostAddress();
 			area.append(ip + "님 접속\n");
+
+			// 바이트 기반 , 입력 스트림(듣기)
+			buffr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			// 바이트 기반 , 출력 스트림(말하기)...
+			buffw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+			//클라이언트가 보낸 메시지 듣기
+			while(true) {
+			String msg = buffr.readLine();
+			
+			area.append(msg+"\n"); //서버에 로그 남기기.
+			buffw.write(msg + "\n"); //버퍼기반의 스트링이므로 문자열의 끝을 알려주지 않으면 , 무한 대기에 빠짐.
+			
+			buffw.flush();
+			}
 			
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
@@ -70,17 +91,17 @@ public class EchoGUIServer extends JFrame implements Runnable{
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void run() {
 		runServer();
 	}
-	
-	//우리가 실행부라고 알고 있었던 존재가 사실 메인 쓰레드라 불리는 프로그램 운영 쓰레드이다.
-	//아이폰 , 안드로이드폰 , 기타 프로그램을 운영하는 메인쓰레드에게 금기시 하는 것이 있음.
-	//1) 무한 루프2) 대기 상태 (accept() , read())
-	//메인 쓰레드를 대기 상태에 빠뜨리면 , 이벤트 감지 , GUI 그래픽 처리 불가 (모든 것이 멈춤.)
+
+	// 우리가 실행부라고 알고 있었던 존재가 사실 메인 쓰레드라 불리는 프로그램 운영 쓰레드이다.
+	// 아이폰 , 안드로이드폰 , 기타 프로그램을 운영하는 메인쓰레드에게 금기시 하는 것이 있음.
+	// 1) 무한 루프2) 대기 상태 (accept() , read())
+	// 메인 쓰레드를 대기 상태에 빠뜨리면 , 이벤트 감지 , GUI 그래픽 처리 불가 (모든 것이 멈춤.)
 	public static void main(String[] args) {
 		new EchoGUIServer();
 	}
-	
+
 }
