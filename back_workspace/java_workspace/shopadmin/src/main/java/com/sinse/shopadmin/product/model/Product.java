011aa431d6f59ce4cbf60ede658c8fc0edb18f3b
DESKTOP-D1BUJAF\a <wlsgksvheh1@naver.com>
2025-06-12 10:13:00 +0900

    1교시 끝 커밋

--- back_workspace/java_workspace/shopadmin/src/main/java/com/sinse/shopadmin/product/model/Product.java
+++ back_workspace/java_workspace/shopadmin/src/main/java/com/sinse/shopadmin/product/model/Product.java
@@ -4,11 +4,70 @@ import java.util.List;
 
 //멤버변수가 db 컬럼명과 일치하도록 한다.
 public class Product {
+	
 	private int product_id;
 	private String product_name;
 	private String brand;
 	private int price;
 	private int discount;
+	private String introduce;
+	private String detail;
 	private List<Color> colorList; //보유색상
+	private List<Size> sizeList; //보유사이즈
 	
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getDiscount() {
+		return discount;
+	}
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+	public String getIntroduce() {
+		return introduce;
+	}
+	public void setIntroduce(String introduce) {
+		this.introduce = introduce;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+	public List<Color> getColorList() {
+		return colorList;
+	}
+	public void setColorList(List<Color> colorList) {
+		this.colorList = colorList;
+	}
+	public List<Size> getSizeList() {
+		return sizeList;
+	}
+	public void setSizeList(List<Size> sizeList) {
+		this.sizeList = sizeList;
+	}
+
 }
