c99d8d78358ca188df0e86184d20d8e45362bfba
DESKTOP-D1BUJAF\a <wlsgksvheh1@naver.com>
2025-07-01 11:16:50 +0900

    2교시 끝 커밋

---
+++ backend/javaee_workspace/BoardApp/src/main/java/com/sinse/boardapp/controller/RegistServlet.java
@@ -0,0 +1,15 @@
+package com.sinse.boardapp.controller;
+
+import javax.servlet.http.HttpServlet;
+
+import com.sinse.boardapp.model.Notice;
+
+
+//클라이언트가 전송한 글쓰기 폼의 파라미터들을 받아 , db에 insert 시키는 서블릿
+public class RegistServlet extends HttpServlet{
+
+	
+	
+	
+	
+}
