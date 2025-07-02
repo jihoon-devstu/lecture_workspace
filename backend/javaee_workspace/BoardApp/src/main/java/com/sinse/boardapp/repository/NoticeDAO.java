6539033ab3b032e5e6df16c7c90691b27674ab1a
DESKTOP-D1BUJAF\a <wlsgksvheh1@naver.com>
2025-07-02 14:31:11 +0900

    alias로 매핑 최적화 완료

--- backend/javaee_workspace/BoardApp/src/main/java/com/sinse/boardapp/repository/NoticeDAO.java
+++ backend/javaee_workspace/BoardApp/src/main/java/com/sinse/boardapp/repository/NoticeDAO.java
@@ -20,7 +20,7 @@ public class NoticeDAO {
 	// 모든 레코드 가져오기
 	public List selectAll() {
 		SqlSession sqlSession = config.getSqlSession();
-		List list = sqlSession.selectList("com.sinse.boardapp.model.Notice.selectAll");
+		List list = sqlSession.selectList("Notice.selectAll");
 		sqlSession.close();
 		return list;
 	}
@@ -28,7 +28,7 @@ public class NoticeDAO {
 	//한건 가져오기
 	public Notice select(int notice_id) {
 		SqlSession sqlSession = config.getSqlSession();
-		Notice notice = sqlSession.selectOne("com.sinse.boardapp.model.Notice.select", notice_id);
+		Notice notice = sqlSession.selectOne("Notice.select", notice_id);
 		sqlSession.close();
 		
 		return notice;
@@ -37,7 +37,7 @@ public class NoticeDAO {
 	//한건 넣기
 	public void insert(Notice notice) throws NoticeException{
 		SqlSession sqlSession = config.getSqlSession();
-		int result = sqlSession.insert("com.sinse.boardapp.model.Notice.insert", notice);
+		int result = sqlSession.insert("Notice.insert", notice);
 		sqlSession.commit(); //DML의 트랜젝션 확정
 		if(result <1) {
 			throw new NoticeException("등록 실패");
@@ -48,7 +48,7 @@ public class NoticeDAO {
 	//한건 수정
 	public void update(Notice notice)throws NoticeException {
 		SqlSession sqlSession = config.getSqlSession();
-		int result = sqlSession.update("com.sinse.boardapp.model.Notice.update", notice);
+		int result = sqlSession.update("Notice.update", notice);
 		sqlSession.commit();
 		if(result <1 ) {
 			throw new NoticeException("수정 실패");
@@ -59,7 +59,7 @@ public class NoticeDAO {
 	//한건 삭제
 	public void delete(Notice notice) {
 		SqlSession sqlSession = config.getSqlSession();
-		int result = sqlSession.delete("com.sinse.boardapp.model.Notice.delete", notice);
+		int result = sqlSession.delete("Notice.delete", notice);
 		sqlSession.commit();
 		if(result <1) {
 			throw new NoticeException("삭제 실패");
