ed7fbf5db695c5da438b430f863749e2eaf36e91
DESKTOP-D1BUJAF\a <wlsgksvheh1@naver.com>
2025-07-04 15:41:04 +0900

    오후 수업 도중 커밋

--- backend/javaee_workspace/memberapp/src/main/java/com/sinse/memberapp/controller/LoginServlet.java
+++ backend/javaee_workspace/memberapp/src/main/java/com/sinse/memberapp/controller/LoginServlet.java
@@ -1,14 +1,25 @@
 package com.sinse.memberapp.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class LoginServlet extends HttpServlet{
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.sinse.memberapp.exception.MemberNotFoundException;
+import com.sinse.memberapp.model.Member;
+import com.sinse.memberapp.repository.MemberDAO;
+
+public class LoginServlet extends HttpServlet{
+	
+	Logger logger = LoggerFactory.getLogger(getClass());
+
+	MemberDAO memberDAO = new MemberDAO();
 	//post로 전송되어 오는 로그인 정보를 받아 , 회원 테이블과 일치 여부에 따라
 	//로그인 성공 및 실패...
 	
@@ -17,9 +28,37 @@ public class LoginServlet extends HttpServlet{
 		String id = request.getParameter("id");
 		String pwd=request.getParameter("pwd");
 		
-		System.out.println(id);
-		System.out.println(pwd);
+		for(int i =1; i<=10; i++) {
+			logger.trace("i는" +i);
+		}
 		
-	
+		logger.debug("넘겨받은 아아디 " +id);
+		logger.debug("넘겨받은 비밀번호 "+pwd);
+		
+		Member member = new Member();
+		
+		member.setId(id);
+		member.setPwd(pwd);
+		
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		Member obj=null;
+		out.print("<script>");
+		try {
+			obj = memberDAO.login(member);
+			out.print("alert('"+obj.getName()+"님 반갑습니다');");
+			out.print("location.href='/index.jsp';");
+		} catch (MemberNotFoundException e) {
+			e.printStackTrace();
+			out.print("alert('"+e.getMessage()+"');");
+			out.print("history.back();");
+		}
+		out.print("</script>");
+//		logger.trace("trace 레벨 출력");
+//		logger.debug("debug 레벨 출력");
+//		logger.info("info 레벨 출력");
+//		logger.warn("warn 레벨 출력");
+//		logger.error("error 레벨 출력");
+		
 	}
 }
--- backend/javaee_workspace/memberapp/src/main/java/com/sinse/memberapp/repository/MemberDAO.java
+++ backend/javaee_workspace/memberapp/src/main/java/com/sinse/memberapp/repository/MemberDAO.java
@@ -2,6 +2,7 @@ package com.sinse.memberapp.repository;
 
 import org.apache.ibatis.session.SqlSession;
 
+import com.sinse.memberapp.exception.MemberNotFoundException;
 import com.sinse.memberapp.model.Member;
 import com.sinse.memberapp.mybatis.MybatisConfig;
 
@@ -9,10 +10,14 @@ public class MemberDAO {
 	
 	MybatisConfig config = MybatisConfig.getInstance();
 	
-	public Member login(Member member) {
+	public Member login(Member member) throws MemberNotFoundException{
 		
 		SqlSession sqlSession = config.getSqlSession();
 		Member obj = sqlSession.selectOne("Member.login",member);
+		
+		if(obj == null) {
+			throw new MemberNotFoundException("로그인 정보가 일치하지 않습니다");
+		}
 		sqlSession.close();
 		return obj;
 	}
