15ff92eaea39305287270859693d195bdb256d2e
jihoon-devstu <https://github.com/jihoon-devstu>
2025-08-25 10:29:08 +0900

    8/25 수업 시작

---
+++ boot_workspace/electroshop/src/main/java/com/sinse/electroshop/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.sinse.electroshop.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "electro/index";
+    }
+
+}
